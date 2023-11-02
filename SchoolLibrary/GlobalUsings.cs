1d4a2a5bc544f56b5cb0202baddd732cd4d35d53
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-11-02 11:18:22 -0700

    initial

---
+++ SchoolLibrary/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using SchoolLibrary.School;
+global using CsvHelper.Configuration;
+global using CsvHelper;
+global using System.Globalization;
+
---
+++ ServerSideBlazor/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using SchoolLibrary.School;
+
+global using Microsoft.EntityFrameworkCore;
+
+global using ServerSideBlazor.Services;
+global using SchoolLibrary.Data;
